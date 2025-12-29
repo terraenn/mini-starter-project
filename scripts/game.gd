5ce643538a2fd996962ce2360cdf46dade6d934a
terraenn <infistorm@duck.com>
2025-12-29 22:09:30 +0200

    File structure stuff

---
+++ scripts/game.gd
@@ -0,0 +1,12 @@
+class_name Game extends Node
+
+#region VARIABLES, SIGNALS AND CONST
+#endregion
+
+#region BUILT-IN
+func _enter_tree() -> void:
+	Global.game = self
+#endregion
+
+#region CLASS
+#endregion
---
+++ scripts/world.gd
@@ -0,0 +1,12 @@
+class_name World extends Node2D
+
+#region VARIABLES, SIGNALS AND CONST
+#endregion
+
+#region BUILT-IN
+func _enter_tree() -> void:
+	Global.world = self
+#endregion
+
+#region CLASS
+#endregion
