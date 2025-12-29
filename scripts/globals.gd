a4ce4e918ba916f3e3937e5419bb3296cfbcaaee
terraenn <infistorm@duck.com>
2025-12-29 22:57:37 +0200

    Stuff

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,4 +1,9 @@
+# Global
 extends Node
 
+## World and UI canvas layer root
 var game : Game
+## 2D world root
 var world : World
+## Container for UI elements
+var ui : Control
---
+++ scripts/signal_bus.gd
@@ -0,0 +1,7 @@
+# SignalBus
+extends Node
+
+@warning_ignore_start("unused_signal")
+signal paused
+signal unpaused
+@warning_ignore_restore("unused_signal")
